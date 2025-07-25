69533e08ca81df5cea831bf8cb83d8c3151817de
Ahmed Tarek Alhusainy <ahmdlhoseny@gmail.com>
2025-07-25 18:39:41 +0300

    Add 8Lab Package Management script

---
+++ 1LinuxLabs/8Lab_PackageManagment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+sudo apt update 
+sudo apt install tree
+sudo apt search tree
+which tree
+tree 
+tree --help
+tree --version
+tree -d
+tree -f
+tree -a
+tree -h
+tree -L 2
+tree -I "*.txt"
+tree -P "*.sh"
+apt-cache policy tree
