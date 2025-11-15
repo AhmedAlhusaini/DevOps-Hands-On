c715ff80eca86b4ed9fe612fc53dd7276f46d4dd
Ahmed Tarek Alhusainy <75701876+AhmedAlhusaini@users.noreply.github.com>
2025-11-15 17:10:52 +0200

    Remove sensitive data from main.tf
    Removed sensitive information from the Vault provider configuration.

--- 7Terraform/getstarted/task-13-terraform-security-vault/terraform_vault/main.tf
+++ 7Terraform/getstarted/task-13-terraform-security-vault/terraform_vault/main.tf
@@ -1,15 +1,15 @@
 /*
 vault version
 vault server -dev
-export VAULT_ADDR="http://127.0.0.1:8200"  >>>>>> linux or MAC
-set VAULT_ADDR="http://127.0.0.1:8200"  >>>>>> windows CMD
+export VAULT_ADDR=""  >>>>>> linux or MAC
+set VAULT_ADDR=""  >>>>>> windows CMD
 vault login <root token>
 vault kv put /secret/app phone_number=867-5309
 */
 
 provider "vault" {
-  address = "http://127.0.0.1:8200"
-  token   = "hvs.aGyvpSsW6jRSfPKfkxMQhr5H"
+  address = ""
+  token   = ""
 }
 data "vault_generic_secret" "phone_number" {
   path = "secret/app"
